747425c532a3949b3a2fd7bbe2d74e0f4c7afcd0
SiegfredLorelle <siegfredlorellecmina@iskolarngbayan.pup.edu.ph>
2024-07-24 05:31:36 +0800

    Add header of courses page

--- lib/screens/courses.dart
+++ lib/screens/courses.dart
@@ -31,26 +31,42 @@ class CoursesPage extends StatelessWidget {
 class CoursesPageHeader extends StatelessWidget {
   const CoursesPageHeader({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 60.0, // Set desired height
       color: Colors.white70,
       padding: const EdgeInsets.fromLTRB(16.0, 20.0, 16.0, 8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
+      child: Stack(
         children: [
-          IconButton(
-            icon: const FaIcon(FontAwesomeIcons.bell, size: 20),
-            color: const Color.fromRGBO(13, 18, 28, 0.867),
-            onPressed: () {},
+          Align(
+            alignment: Alignment.bottomRight,
+            child: IconButton(
+              icon: const FaIcon(FontAwesomeIcons.angleDown, size: 20),
+              color: const Color.fromRGBO(13, 18, 28, 0.867),
+              onPressed: () {
+                // Define your action here
+              },
+            ),
+          ),
+          Center(
+            child: Text(
+              'Challenge',
+              style: TextStyle(
+                fontSize: 24.0,
+                color: const Color.fromRGBO(13, 18, 28, 0.867),
+                fontFamily: "Lexend",
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
         ],
       ),
     );
   }
 }
+
+
 
 class CoursesPageBody extends StatelessWidget {
   const CoursesPageBody({super.key});
