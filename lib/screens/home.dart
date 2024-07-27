5a55def69ce479d01602345dabd65deaa392030c
SiegfredLorelle <siegfredlorellecmina@iskolarngbayan.pup.edu.ph>
2024-07-27 21:48:32 +0800

    Update colors in pretest page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../widgets/home-image-carousel.dart';
-import '../screens/login.dart';
 import '../game/view/game_view.dart'; // Import the game screen
 
 class HomePage extends StatelessWidget {
