f69f608b8b92d8256fda9683b8d9acce8869dc4a
Gaeus Caskie <95890979+OvertheSkyy@users.noreply.github.com>
2024-07-30 00:40:02 +0800

    Revert back to previous main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:EduGAIte/audio/audio.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'firebase_options.dart';
 import 'screens/home.dart';
 import 'screens/login.dart';
@@ -14,16 +17,21 @@ import 'screens/courses.dart';
 import 'screens/lessons/vid_lesson.dart';
 import 'screens/signup.dart';
 import 'screens/lessons/pretest.dart';
-import 'package:EduGAIte/game/bloc/navigation_bloc.dart';
-import 'package:EduGAIte/game/bloc/navigation_state.dart';
-
-final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+import 'screens/lessons/chat_lesson.dart';
+import 'screens/lessons/chatbot.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Load environment variables from the root .env file
+  await dotenv.load(fileName: ".env");
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseFirestore.instance.settings = const Settings(
+    persistenceEnabled: true,
   );
 
   runApp(const MyApp());
@@ -38,46 +46,40 @@ class MyApp extends StatelessWidget {
       providers: [
         Provider(create: (_) => AudioController()),
         BlocProvider(create: (_) => GameBloc()),
-        BlocProvider(create: (_) => NavigationBloc()),
       ],
-      child: BlocListener<NavigationBloc, NavigationState>(
-        listener: (context, state) {
-          final navigator = navigatorKey.currentState!;
-          if (state is CoursePageState) {
-            navigator.pushNamed('/courses');
-          }
+      child: MaterialApp(
+        initialRoute: "/",
+        routes: {
+          "/": (context) => const HomePage(),
+          "/login": (context) => const LoginPage(),
+          "/signup": (context) => const SignUpPage(),
+          "/game": (context) => const GameScreen(),
+          "/profile": (context) => const ProfilePage(),
+          "/track": (context) => const TrackPage(),
+          "/courses": (context) => const CoursesPage(),
+          "/courses/pretest": (context) => const PretestPage(),
+          "/courses/vid_lesson": (context) => const VidLessonPage(),
+          "/courses/chat_lesson": (context) => const ChatLessonPage(),
+          "/courses/chatbot": (context) => const ChatbotPage(),
         },
-        child: MaterialApp(
-          navigatorKey: navigatorKey,
-          initialRoute: "/",
-          routes: {
-            "/": (context) => const HomePage(),
-            "/login": (context) => const LoginPage(),
-            "/signup": (context) => const SignUpPage(),
-            "/game": (context) => const GameScreen(),
-            "/profile": (context) => const ProfilePage(),
-            "/track": (context) => const TrackPage(),
-            "/courses": (context) => const CoursesPage(),
-            "/courses/pretest": (context) => const PretestPage(),
-            "/courses/vid_lesson": (context) => const VidLessonPage(),
-          },
-          title: 'EduGAIte',
-          theme: ThemeData(
-            colorScheme: const ColorScheme(
-              primary: Colors.white70,
-              primaryContainer: Colors.white70,
-              secondary: Colors.white70,
-              secondaryContainer: Colors.white70,
-              surface: Colors.white,
-              error: Colors.red,
-              onPrimary: Color.fromRGBO(13, 18, 28, 0.867),
-              onSecondary: Color.fromRGBO(13, 18, 28, 0.867),
-              onSurface: Colors.black,
-              onError: Colors.white,
-              brightness: Brightness.light,
-            ),
-            useMaterial3: true,
+        title: 'EduGAIte',
+        theme: ThemeData(
+          colorScheme: const ColorScheme(
+            primary: Colors.white70, // Primary color
+            primaryContainer: Colors.white70, // Primary container color
+            secondary: Colors.white70, // Secondary color
+            secondaryContainer: Colors.white70, // Secondary container color
+            surface: Colors.white, // Surface color
+            error: Colors.red, // Error color
+            onPrimary: Color.fromRGBO(
+                13, 18, 28, 0.867), // Text color on primary color
+            onSecondary: Color.fromRGBO(
+                13, 18, 28, 0.867), // Text color on secondary color
+            onSurface: Colors.black, // Text color on surface color
+            onError: Colors.white, // Text color on error color
+            brightness: Brightness.light, // Brightness of the overall theme
           ),
+          useMaterial3: true,
         ),
       ),
     );
